03a9bdcb786d59f4f58aa526e3ceb4688422f5d4
1995ankita <ankita.hirpara@theknowledgeacademy.com>
2024-05-09 14:59:15 +0530

    counter with redux part 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import Container from './components/Container';
 import Controls from './components/Controls';
 import DisplayCounter from './components/DisplayCounter';
 import Header from './components/Header';
+import PrivacyMessage from './components/PrivarcyMessage';
+import { useSelector } from "react-redux";
 
 function App() {
+  const privacy = useSelector((store) => store.privacy);
   return (
    
       <center className="px-4 py-5 my-5 text-center">
         <Container>
         <Header></Header>
         <div className="col-lg-6 mx-auto">
-         <DisplayCounter></DisplayCounter>
+        { privacy ? <PrivacyMessage /> : <DisplayCounter />}
          <Controls></Controls>
         </div>
         </Container>
