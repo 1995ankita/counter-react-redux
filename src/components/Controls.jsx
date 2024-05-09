29e9e535797c80f16431790f999ea95c5f398fa3
1995ankita <ankita.hirpara@theknowledgeacademy.com>
2024-05-09 13:48:55 +0530

    counter

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,7 +1,19 @@
-const Controls = () =>{
-    return  <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-    <button type="button" className="btn btn-primary btn-lg px-4 gap-3">+1</button>
-    <button type="button" className="btn btn-outline-secondary btn-lg px-4">-1</button>
+import { useDispatch } from "react-redux";
+
+const Controls = () => {
+  const dispatch = useDispatch();
+
+  const HandleIncrement = () => {
+    dispatch({ type: "INCREMENT" });
+
+  };
+  const HandleDecrement = () => {
+    dispatch({ type: "DECREMENT" });
+
+  }
+  return <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+    <button type="button" className="btn btn-primary btn-lg px-4 gap-3" onClick={HandleIncrement}>+1</button>
+    <button type="button" className="btn btn-outline-secondary btn-lg px-4" onClick={HandleDecrement}>-1</button>
   </div>
 }
 export default Controls;
